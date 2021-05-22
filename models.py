b14216c98b32dac287dc547f5784bba8eea0e51c
daeun-j <daeun0710@gmail.com>
2021-05-22 20:58:59 +0900

    DNN 0522(3)

--- models.py
+++ models.py
@@ -4,6 +4,25 @@ import torch.nn as nn
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
+
+class RNNModel_CUDA(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(RNNModel_CUDA, self).__init__()
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+        self.rnn = nn.RNN(input_dim, hidden_dim, layer_dim, batch_first=True, nonlinearity='relu')
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_().to(device)
+        out, hn = self.rnn(x, h0.detach())
+        out = self.fc(out[:, -1, :])
+
+        return out
+
 class LSTM_v0(nn.Module):
     def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
         super(LSTM_v0, self).__init__()
